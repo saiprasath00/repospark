1492e0f75936624c19e7c4c9edb6832c4857f8ea
saiprasath00 <207870010+saiprasath00@users.noreply.github.com>
2026-05-02 23:07:46 +0530

    Add Android APK build

---
+++ android/app/src/main/java/com/saiprasath/spooty/MainActivity.java
@@ -0,0 +1,38 @@
+package com.saiprasath.spooty;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private static final String APP_URL = "file:///android_asset/public/index.html";
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(true);
+        settings.setMixedContentMode(WebSettings.MIXED_CONTENT_COMPATIBILITY_MODE);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(APP_URL);
+        setContentView(webView);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView != null && webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
